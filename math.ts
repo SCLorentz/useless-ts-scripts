9546480e27e10ecd12c735aaa0966d2cd62fc9ec
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-09-01 11:09:22 -0400

    Update math.ts

--- math.ts
+++ math.ts
@@ -1,22 +1,25 @@
+// this is a script that can be used for math
+// If you can easily use the math operators in high level languages, like ts... Why not use an alternative method?
+
 const calc = {
-    '+': (op: Array<number>) => op.reduce((x, y) => x + y),
-    '-': (op: Array<number>) => op.reduce((x, y) => x - y),
-    '*': (op: Array<number>) => op.reduce(function m(x, y): number {
-      if (y == 0 || y == 0) return 0;
+    '+': (op: number[]) => op.reduce((x, y) => x + y),
+    '-': (op: number[]) => op.reduce((x, y) => x - y),
+    '*': (op: number[]) => op.reduce(function m(x, y): number {
+      if (y == 0) return 0;
       if (y < 0) return -m(x, -y);
       return x + m(x, y - 1);
     }),
-    '/': (op: Array<number>) => op.reduce(function d(x, y): number {
+    '/': (op: number[]) => op.reduce(function d(x, y): number {
       if (x == 0 || x < y || y == 0) return 0;
       if (y < 0) return -d(x, -y);
       return 1 + d(x - y, y);
     })
 };
 
-const som = calc['+']([1, 2, 3, 4]);
-const mult = calc['*']([5, 2]);
-const div = calc['/']([60, 2, 3]);
-const negative = calc['*']([10,-1]);
+const som = calc['+']([1, 2, 3, 4]),
+    mult = calc['*']([5, 2]),
+    div = calc['/']([60, 2, 3]),
+    negative = calc['*']([10,-1]);
 
 console.log(som)
 console.log(mult)
