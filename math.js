b82827f040480b5be062cd17e2d5d951f08aa3e5
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-09-01 10:49:54 -0400

    Create math.js

---
+++ math.js
@@ -0,0 +1,24 @@
+const calc = {
+    '+': (op) => op.reduce((x, y) => x + y),
+    '-': (op) => op.reduce((x, y) => x - y),
+    '*': (op) => op.reduce(function m(x, y) {
+      if (y == 0 || y == 0) return 0;
+      if (y < 0) return -m(x, -y);
+      return x + m(x, y - 1);
+    }),
+    '/': (op) => op.reduce(function d(x, y) {
+      if (x == 0 || x < y || y == 0) return 0;
+      if (y < 0) return -d(x, -y);
+      return 1 + d(x - y, y);
+    })
+};
+
+const som = calc['+']([1, 2, 3, 4]);
+const mult = calc['*']([5, 2]);
+const div = calc['/']([60, 2, 3]);
+const negative = calc['*']([10,-1]);
+
+console.log(som)
+console.log(mul)
+console.log(div)
+console.log(negative)
