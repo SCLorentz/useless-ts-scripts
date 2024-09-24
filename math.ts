f100445ed8c163654b73ac0a9e3ae37062de2180
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-09-24 10:04:31 -0300

    Update math.ts

--- math.ts
+++ math.ts
@@ -7,19 +7,21 @@ const calc = {
     '*': (op: number[]) => op.reduce(function m(x, y): number {
       if (y == 0) return 0;
       if (y < 0) return -m(x, -y);
-      return x + m(x, y - 1);
+      //
+      return x + m(x, calc['-']([y, 1]))
     }),
     '/': (op: number[]) => op.reduce(function d(x, y): number {
       if (x == 0 || x < y || y == 0) return 0;
       if (y < 0) return -d(x, -y);
-      return 1 + d(x - y, y);
+      //
+      return 1 + d(calc['-']([x, y]), y)
     })
 };
 
 const som = calc['+']([1, 2, 3, 4]),
     mult = calc['*']([5, 2]),
     div = calc['/']([60, 2, 3]),
-    negative = calc['*']([10,-1]);
+    negative = calc['*']([10, -1]);
 
 console.log(som)
 console.log(mult)
