a2eb091753380961de5c92ac21bf0733592be9fe
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-09-25 08:12:17 -0300

    Update overcomplicated_math.ts

--- overcomplicated_math.ts
+++ overcomplicated_math.ts
@@ -5,52 +5,57 @@ class Calc {
   private op: number[];
   private type: string;
   //
-  public constructor(type: string, op: number[]) {
-    this.op = op;
+  public constructor(type: string) {
     this.type = type;
   }
-  public result() {
+  //
+  public result(op: number[]) {
+    this.op = op;
+    //
     return this.calc[this.type].bind(this)(this.op);
   }
   //
-  /*private is_zero(x: number, y: number): number {
-    return (y == 0 || x == 0) ? 0 : NaN
-  }*/
+  private is_zero(x: number, y: number): boolean {
+    return (y == 0 || x == 0) ? true : false
+  }
+  //
+  private div(x: number, y: number): number {
+    //
+    if (x == y) return 1
+    if (y < 0) return this.calc['*']([this.div(x, -y), -1]);
+    //
+    return this.is_zero(x, y) || x < y ? 0 : this.calc['+']([this.div(x - y, y), 1])
+  }
+  private mult(x: number, y: number): number {
+    //
+    if (x == 1) return y
+    if (y < 0) return -this.mult(x, -y);
+    //
+    return (y == 0) ? 0 : x + this.mult(x, this.calc['-']([y, 1]))
+  }
   //
   public calc = {
     // soma
     '+': (op: number[]) => op.reduce((x, y) => x + y),
     // subtração
-    '-': (op: number[]) => op.reduce((x, y) => x - y),
+    '-': (op: number[]) => op.reduce((x, y) => this.calc['+']([x, this.calc['*']([y, -1])])),
     // multiplicação
-    '*': (op: number[]) => op.reduce(function mult(x, y): number {
-      //
-      if (x == 1) return y
-      if (y < 0) return -mult(x, -y);
-      //
-      return (y == 0) ? 0 : x + mult(x, y - 1)
-    }),
+    '*': (op: number[]) => op.reduce((x, y) => this.mult(x, y)),
     // divisão
-    '/': (op: number[]) => op.reduce(function div(x, y): number {
-      //
-      if (x == y) return 1
-      if (y < 0) return -div(x, -y);
-      //
-      const is_zero = x == 0 || x < y || y == 0;
-      //
-      return is_zero ? 0 : 1 + div(x - y, y)
-    })
+    '/': (op: number[]) => op.reduce((x, y) => this.div(x, y))
   };
 }
 
 const results = [
-  new Calc('+', [1, 2, 3, 4]).result(), // sum
+  new Calc('+').result([1, 2, 3, 4]), // sum
   //
-  new Calc('*', [5, 2]).result(),       // multiplication
-  new Calc('*', [10, -1]).result(),     // negative result
+  new Calc('*').result([5, 2]),       // multiplication
+  new Calc('*').result([10, -1]),     // negative result
   //
-  new Calc('/', [60, 2, 3]).result(),   // division
-  new Calc('/', [40, 40]).result(),     // division by itself is always 1
+  new Calc('/').result([60, 2, 3]),   // division
+  new Calc('/').result([40, 40]),     // division by itself is always 1
+  //
+  new Calc('-').result([20, 10])
 ]
 
 results.forEach(e => console.log(e))
