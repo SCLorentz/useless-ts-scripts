c0869a63a0159c4666621ea51155f41b12fb2b83
S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-10-19 12:28:23 -0400

    Update overcomplicated_math.ts

--- overcomplicated_math.ts
+++ overcomplicated_math.ts
@@ -98,18 +98,15 @@ results.forEach(e => console.log(e))
 
 ///////////////////////////////////////////////////////////////////////////////////
 
-function bin(x: number, bits: number): number
+function bin(x: number, bits: number, y = x.toString(2)): number
 {
-  const inversed_binary = (y = x.toString(2)) => {
-    while (y.length < bits) y = `0${y}`; // use a recursive call here
-    return y
-  }
+  const binary: string = Array(bits - y.length + 1).join("0") + y;
 
   const next = (x: string): string => {
     return x.charAt(0) == "0" ? `1${x.substring(1)}` : `0${next(x.slice(1))}`
   }
 
-  const result = reverse_string(next(reverse_string(inversed_binary())));
+  const result = reverse_string(next(reverse_string(binary)));
 
   return parseInt(result, 2);
 }
@@ -126,21 +123,3 @@ function reverse_string(value: string) {
 for (let i = 0; i < 100; i++) {
   console.log(`${i} -> ${bin(i, 7)}`)
 }
-
-// 07 --> 0111 - 1110
-// 08 --> 1000 - 0001
-// 09 --> 1001 - 1001
-// 10 --> 1010 - 0101
-// 11 --> 1011 - 1101
-
-// 1 -> return 0 and continue to the next one
-// 0 -> return 1, stop and merge with the remaining
-
-// (11) 1101 --> A >> 1, so return 0 and continue
-// 101 --> B >> 1, so return 0 and continue
-// 01 --> C >> 0, so return 1 and merge with the unmodfied part
-// (12) 0011
-
-// 1101 -> 0
-// 101 -> 0
-// 01 -> 1 stop
