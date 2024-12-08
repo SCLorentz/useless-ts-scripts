0dba1556d07f887f7ad4d45fa8d98e3c8ef1b41f
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-12-08 00:11:48 +0000

    wirdo

--- weirdo.ts
+++ weirdo.ts
@@ -22,3 +22,19 @@ T: { while(true.invert().toString() == "false")
     console.log("hello world!")
     break T
 }}
+
+let _x = "bruh" as const;
+
+F: {while (false)
+{
+    if (_x == "bruh")
+    {
+        console.log("hello world!")
+        _x = "other"
+        break F
+    }
+    else 
+    {
+        _x = "bruh"
+    }
+}}
